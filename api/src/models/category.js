1e7018d0c9df26d432703ac45ffb6cff8a417be8
Carlos Cruz <carturo99@gmail.com>
2022-08-16 17:11:21 -0500

    agregando el modelo category

---
+++ api/src/models/category.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('pokemon', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+
+//Prueba de GitFlow
