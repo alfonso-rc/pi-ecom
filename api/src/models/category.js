bae84fd10e303735a90f41e4876504766e0d444e
Alejandro Silva <alejoyodax@gmail.com>
2022-08-17 17:14:05 -0500

    fix(id de modelo category)

--- api/src/models/category.js
+++ api/src/models/category.js
@@ -2,9 +2,9 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define('category', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
+    id: { // Solo tendremos 4 categorias de productos
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
     name: {
       type: DataTypes.TEXT,
