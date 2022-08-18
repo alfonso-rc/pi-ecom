5ae4966d32db9e1974a16e9f15d58d21ebd06ac4
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-08-17 19:23:32 -0500

    Delete RouteCategory.js

--- api/src/routes/RouteCategory.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const {testFunction} = require('../Controllers/CategoryControl.js');
-
-const CategoryRouter = Router();
-
-CategoryRouter.get('/',testFunction);
-
-module.exports = CategoryRouter;
