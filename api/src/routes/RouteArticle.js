9c829f6e457341fcf7460a360c4931636bc8c58d
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-08-17 19:23:25 -0500

    Delete RouteArticle.js

--- api/src/routes/RouteArticle.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const {testFunction} = require('../Controllers/CategoryControl.js');
-
-const articleRouter = Router();
-
-articleRouter.get('/',testFunction);
-
-module.exports = articleRouter;
