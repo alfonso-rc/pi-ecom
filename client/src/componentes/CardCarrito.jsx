d7ec9e0f1303d2242fed5599e6587fc6bbe11cf2
nicolaszacaria <zacariasnicolas95@gmail.com>
2022-08-24 18:44:07 -0300

    carrito

---
+++ client/src/componentes/CardCarrito.jsx
@@ -0,0 +1,35 @@
+import { removeCart } from "../store/actions";
+import { useSelector, useDispatch } from "react-redux";
+import {MdDeleteForever} from "react-icons/md";
+
+export default function CardCarrito({id, title, stock, image, price, category}){
+const dispatch = useDispatch()
+function handleDelete(id){
+dispatch(removeCart(id))
+}
+return (
+    <div>
+        <div>
+            <img src={image} alt="img not found" height={180} width={120}/>
+        </div>
+        <div>
+            <p>{title}</p>
+        </div>
+            <div>
+                <p>{price}</p>
+            </div>
+            <div>
+                <p>{stock}</p>
+            </div>
+            <abbr>
+       <span  onClick={() => handleDelete(id)}>
+        <MdDeleteForever />
+        </span>
+        </abbr>
+    </div>
+
+)
+
+
+
+}
