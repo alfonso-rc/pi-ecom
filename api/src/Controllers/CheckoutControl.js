6c3b269e2c27162e53e1302297127d9a4c5ac010
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-08-24 14:54:17 -0500

    check

--- api/src/Controllers/CheckoutControl.js
+++ api/src/Controllers/CheckoutControl.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require("express");
 const Stripe = require("stripe");
-
-const {
- STRIPE_PUBLIC
-} = process.env;
-
+const { STRIPE_PUBLIC} = process.env;
+//const Stripe= require("stripe")
 const stripe = new Stripe("sk_test_51LYz7FBeAsr6kwMLC7RfoW2QsnT6AKhEYBGECdCkAHvXbXkkGzZFXaBW5YfaQYySP0otxZMXUWESRYaKaaEq9QSv00yiIMxNMm");
+//const {mandarEmail}= require("..utils/sendEmail");
 
 const testFunction = (req, res, next) => {
     try {
@@ -24,11 +22,11 @@ const testFunction = (req, res, next) => {
     const payment = await stripe.paymentIntents.create({
       amount,
       currency: "USD",
-      description: "Gaming Keyboard",
+      description: "Compraste productos en Ecom",
       payment_method: id,
       confirm: true, //confirm the payment at the same time
-    });
-
+    })
+    //mandarEmailCompra(amount,currency, description);
     console.log(payment);
 
     return res.status(200).json({ message: "Successful Payment" });
