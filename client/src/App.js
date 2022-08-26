da9e67f450d2fc56e6ad9d96c35fb3003370371c
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-08-26 14:59:04 -0500

    cambios y notfound

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./componentes/Home";
 import ArticleDetail from "./componentes/Detail";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 import LandingPage from "./componentes/LandingPage";
 import Example from "./componentes/Login";
 import NewUser from "./componentes/newUser";
@@ -10,12 +10,14 @@ import AdminHome from "./componentes/Admin/Pages/AdminHome";
 import ArticleList from "./componentes/Admin/Pages/ArticleList"
 import UserList from "./componentes/Admin/Pages/UserList"
 import Offer from "./componentes/Admin/Pages/Offer";
+import NotFound from'./componentes/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
+          
           <Route path= "/checkout" component={CheckoutForm}/>
           <Route exact path="/newUser" component={NewUser}/>
           <Route exact path="/home" component={Home} />
@@ -24,8 +26,11 @@ function App() {
           <Route exact path="/admin/articulos" component={ArticleList}/>
           <Route exact path="/admin/usuarios" component={UserList}/>
           <Route exact path="/admin/ofertas"  component={Offer}/>
+          
           <Route path="/:id" component={ArticleDetail} />
           <Route exact path="/" component={LandingPage} />
+          <Route path="*" component={NotFound}/>
+         {/*  <Redirect to="/not-found"/> */}
         </Switch>
       </div>
     </BrowserRouter>
