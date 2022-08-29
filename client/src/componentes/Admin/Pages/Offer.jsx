a13447e3963acfd039a9071fab55718300d3c8bb
Juani50 <juanibiondi50@gmail.com>
2022-08-29 11:30:51 -0300

    agrgo nav y footer en admin

--- client/src/componentes/Admin/Pages/Offer.jsx
+++ client/src/componentes/Admin/Pages/Offer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SideBar from "../SideBar";
 import Logo from "../../../../../client/src/ECOM-10_2.png";
+import NavBarAdmin from "../../NavBarAdmin";
+import Footer from "../../Footer";
 
 export default function Offer() {
   return (
     <div className="m-0 p-0">
+      <NavBarAdmin/>
       <img
         className="mx-auto h-32 justify-center w-auto"
         src={Logo}
@@ -17,6 +20,7 @@ export default function Offer() {
           <p>ACA ESTAN LAS OFERTAS</p>
         </div>
       </div>
+      <Footer/>
     </div>
   );
 }
