29650ffc160f27e56bca6114a21ea63144941560
Alejandro Silva <alejoyodax@gmail.com>
2022-08-30 20:58:29 -0500

    fix(client/ detail article): ahora se controla en una variable  el texto de la caja del comentario y se incluye en el body de la peticion POST al hacer un commentario, fix(api/ comment): arreglada la lógica de la funcion que crea comentario en la bd

--- client/src/componentes/Detail.jsx
+++ client/src/componentes/Detail.jsx
@@ -6,10 +6,10 @@ import { IoAdd, IoRemove } from "react-icons/io5";
 import NavBarDetail from "./NavBarDetail";
 import Carrito from "./Carrito";
 import { useDispatch } from "react-redux";
-import { addToCart,addComment, addRating, getUsers } from "../store/actions";
+import { addToCart, addComment, addRating, getUsers } from "../store/actions";
 import Footer from "./Footer";
 import NotFound from "./NotFound";
-import loading  from "../imagenes/loading2.gif"
+import loading from "../imagenes/loading2.gif"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const BASE_URL = process.env.REACT_APP_API_URL;
@@ -22,8 +22,8 @@ export default function ArticleDetail() {
 
   const dispatch = useDispatch();
   const history = useHistory();
-  
-  
+
+
 
   function addCart(item) {
     dispatch(addToCart(item));
@@ -41,8 +41,8 @@ export default function ArticleDetail() {
   }
 
 
- 
-  
+
+
 
 
   const [article, setArticle] = useState(null);
@@ -57,19 +57,19 @@ export default function ArticleDetail() {
     });
   }, []);
 
-  const [input,setInput]=useState({texto:''});
+  const [input, setInput] = useState("");
 
 
   const HandleClickRating = (e, score) => {
     e.preventDefault();
     const token = sessionStorage.getItem("token");
     if (token) {
-        dispatch(addRating({
-    idArticle: article.id,
-     idUser: sessionStorage.getItem("id"),
-    score: score
-        }))
-        alert('Rating Agregado!')
+      dispatch(addRating({
+        idArticle: article.id,
+        idUser: sessionStorage.getItem("id"),
+        score: score
+      }))
+      alert('Rating Agregado!')
     } else {
       toastErrors();
     }
@@ -90,21 +90,25 @@ export default function ArticleDetail() {
   //     }
   //   }
 
-  const HandleClickComment = (e, texto) => {
+  const HandleClickComment = (e) => {
     e.preventDefault();
     const token = sessionStorage.getItem("token");
     if (token) {
-        dispatch(addComment({
-          texto: texto,
-          idUser: sessionStorage.getItem("id"),
-          articleId: article.id
-        }))
-        alert('Comentario Agregado!')
+      dispatch(addComment({
+        texto: input,
+        idUser: sessionStorage.getItem("id"),
+        articleId: article.id
+      }))
+      alert('Comentario Agregado!')
     } else {
       toastErrors();
     }
   };
 
+  const handleTextInputComment = (e) => {
+    console.log(e.target.value)
+    setInput(e.target.value)
+  }
 
 
 
@@ -119,29 +123,29 @@ export default function ArticleDetail() {
         <button class="btn btn-circle btn-outline"></button>
       </div>
       <div className="font-Work text-xl md:text-1xl bg-white text-black pt-40">
-        {article ? (
+        { article ? (
           <div>
             <div className="flex flex-wrap items-center justify-center lg:grid grid-cols-2">
               <div className="justify-center">
                 <img
-                  src={article.image}
+                  src={ article.image }
                   alt="image"
                   className="lg:m-auto h-96 w-auto"
                 />
               </div>
               <div className="lg:m-auto xl:ml-20 pt-6">
                 <div>
-                  <h3 className="font-bold text-4xl">{article.title}</h3>
+                  <h3 className="font-bold text-4xl">{ article.title }</h3>
                   <div className="flex flex-row justify-center pt-6">
                     <h1 className="font-bold">Rating: </h1>
-                    {/* esta es la forma provicional del Rating */}
-                    <p>{article.rating === "NaN" ? article.rating = 0 : article.rating }</p>
+                    {/* esta es la forma provicional del Rating */ }
+                    <p>{ article.rating === "NaN" ? article.rating = 0 : article.rating }</p>
                   </div>
                 </div>
                 <div>
                   <div className="flex flex-row justify-center py-3 font-bold pb-6">
                     <h1>Precio: </h1>
-                    <p className="text-accent font-mono">${article.price}</p>
+                    <p className="text-accent font-mono">${ article.price }</p>
                   </div>
                   <div className="flex justify-center">
                     <button className="btn btn-outline btn-primary btn-sm btn-square">
@@ -154,32 +158,32 @@ export default function ArticleDetail() {
                   </div>
                   <div className="flex flex-row justify-center pt-6">
                     <h1 className="font-bold">Stock: </h1>
-                    <p>{article.stock}</p>
+                    <p>{ article.stock }</p>
                   </div>
                   <div className="flex flex-row justify-center pt-6">
                     <h1 className="font-bold">Color: </h1>
-                    <p>{article.detail.color}</p>
+                    <p>{ article.detail.color }</p>
                   </div>
                   <br />
                   <br />
                   <button
                     className="btn btn-accent btn-wide my-2"
-                    onClick={() => addCart(article)}
+                    onClick={ () => addCart(article) }
                   >
                     Agregar al carrito
                   </button>
                   <br />
-                  {/* <button className="btn btn-primary btn-wide">Comprar</button>                */}
+                  {/* <button className="btn btn-primary btn-wide">Comprar</button>                */ }
                 </div>
                 <div className="rating rating-lg rating-half">
-  <input type="radio" name="rating-7" className="rating-hidden" />
-  <input  type="radio" name="rating-7"  class="mask mask-star-2 bg-orange-400" value={1} onClick={(e)=>HandleClickRating(e, 1)}/>
-  <input type="radio" name="rating-7"  class="mask mask-star-2 bg-orange-400" value={2} onClick={(e)=>HandleClickRating(e, 2)}/>
-  <input type="radio" name="rating-7"  class="mask mask-star-2 bg-orange-400" value={3} onClick={(e)=>HandleClickRating(e, 3)}/>
-  <input  type="radio" name="rating-7"  class="mask mask-star-2 bg-orange-400" value={4} onClick={(e)=>HandleClickRating(e, 4)}/>
-  <input  type="radio" name="rating-7"  class="mask mask-star-2 bg-orange-400" value={5} onClick={(e)=>HandleClickRating(e, 5)}/>
-</div>
-<ToastContainer/>
+                  <input type="radio" name="rating-7" className="rating-hidden" />
+                  <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" value={ 1 } onClick={ (e) => HandleClickRating(e, 1) } />
+                  <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" value={ 2 } onClick={ (e) => HandleClickRating(e, 2) } />
+                  <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" value={ 3 } onClick={ (e) => HandleClickRating(e, 3) } />
+                  <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" value={ 4 } onClick={ (e) => HandleClickRating(e, 4) } />
+                  <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" value={ 5 } onClick={ (e) => HandleClickRating(e, 5) } />
+                </div>
+                <ToastContainer />
               </div>
             </div>
             <br />
@@ -189,35 +193,35 @@ export default function ArticleDetail() {
             <div className="mx-8 text-start lg:px-20">
               <h1 className="mt-14 font-bold text-4xl">Descripcion</h1>
               <br />
-              <p>{article.detail.detail}</p>
+              <p>{ article.detail.detail }</p>
               <h1 className="mt-14 font-bold text-4xl">Especificaciones</h1>
               <br />
               <div className="grid grid-cols-2">
                 <h1 className="font-bold mt-2">Marca:</h1>
-                <p className="mt-2">{article.detail.marca}</p>
-                <h1 className="font-bold mt-8">Modelo:</h1>{" "}
-                <p className="mt-8">{article.detail.modelo} </p>
+                <p className="mt-2">{ article.detail.marca }</p>
+                <h1 className="font-bold mt-8">Modelo:</h1>{ " " }
+                <p className="mt-8">{ article.detail.modelo } </p>
                 {/* Si a alguien le parece que este codigo esta muy feo cosa que a mi tambien y quiere arreglarlo con gusto lo acepto
                                 Comprobamos que descripcion existe y mostramos, pasa que lo hice 1 por uno porque no queria romper el estilo,despues arreglo u.u */}
-                {article.detail.so && <h1 className="font-bold mt-8">SO:</h1>}
-                {article.detail.so && (
-                  <p className="mt-8">{article.detail.so}</p>
-                )}
-                {article.detail.cpu && <h1 className="font-bold mt-8">CPU:</h1>}
-                {article.detail.cpu && (
-                  <p className="mt-8">{article.detail.cpu}</p>
-                )}
-                {article.detail.ram && <h1 className="font-bold mt-8">RAM:</h1>}
-                {article.detail.ram && (
-                  <p className="mt-8">{article.detail.ram}</p>
-                )}
-                {article.detail.pantalla && (
+                { article.detail.so && <h1 className="font-bold mt-8">SO:</h1> }
+                { article.detail.so && (
+                  <p className="mt-8">{ article.detail.so }</p>
+                ) }
+                { article.detail.cpu && <h1 className="font-bold mt-8">CPU:</h1> }
+                { article.detail.cpu && (
+                  <p className="mt-8">{ article.detail.cpu }</p>
+                ) }
+                { article.detail.ram && <h1 className="font-bold mt-8">RAM:</h1> }
+                { article.detail.ram && (
+                  <p className="mt-8">{ article.detail.ram }</p>
+                ) }
+                { article.detail.pantalla && (
                   <h1 className="font-bold mt-8">Pantalla:</h1>
-                )}
-                {article.detail.pantalla && (
-                  <p className="mt-8">{article.detail.pantalla}</p>
-                )}
-                {/* <h1 className="font-bold mt-8">Categoria:</h1><p className="mt-8">{article.categories?article.categories[0].name:article.categories}</p> */}
+                ) }
+                { article.detail.pantalla && (
+                  <p className="mt-8">{ article.detail.pantalla }</p>
+                ) }
+                {/* <h1 className="font-bold mt-8">Categoria:</h1><p className="mt-8">{article.categories?article.categories[0].name:article.categories}</p> */ }
               </div>
             </div>
             <br />
@@ -226,19 +230,19 @@ export default function ArticleDetail() {
               tabIndex="0"
               class="collapse collapse-plus bg-accent rounded-box "
             >
-            <div className="">
-            <div class="collapse-title text-white text-xl font-medium">
-                <h1>Comentarios</h1>
-              </div>
-              <div class="collapse-content bg-white border-4">
-                {
-                  
+              <div className="">
+                <div class="collapse-title text-white text-xl font-medium">
+                  <h1>Comentarios</h1>
+                </div>
+                <div class="collapse-content bg-white border-4">
+                  {
+
                     article.comments.length > 0 ? (
                       article.comments.map((comm) => {
                         return (
                           <div className="flex flex-row justify-between">
-                            <p className="mt-8 border-b-4">{comm.texto}</p>
-                            <p className="mt-8 border-b-4">{comm.user.userName}</p>
+                            <p className="mt-8 border-b-4">{ comm.texto }</p>
+                            <p className="mt-8 border-b-4">{ comm.user.userName }</p>
                           </div>
                         );
                       })
@@ -247,28 +251,29 @@ export default function ArticleDetail() {
                         No hay comentarios sobre este producto
                       </p>
                     )
-                  
-                }
+
+                  }
+                </div>
               </div>
             </div>
-            </div>
 
-              
+
             <br />
             <div className="flex flex-row">
               <textarea
+                onChange={ (e) => handleTextInputComment(e) }
                 class="textarea textarea-accent bg-white  w-5/6 ml-10"
                 placeholder="Add new comment"
                 name="texto"
               ></textarea>
-              <button class="btn btn-outline btn-accent" type= "submit" onClick={(e)=>HandleClickComment(e)}>Post</button>
+              <button class="btn btn-outline btn-accent" type="submit" onClick={ (e) => HandleClickComment(e) }>Post</button>
             </div>
           </div>
         ) : (
           <div className="flex  place-content-center">
-            {<img src={loading} alt="img not found" />}
+            { <img src={ loading } alt="img not found" /> }
           </div>
-        )}
+        ) }
         <br />
         <br />
         <Link to="/home">
