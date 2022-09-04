5ddd949b9df667c7f3f739711bf6792d04ce217d
Alejandro Silva <alejoyodax@gmail.com>
2022-09-03 19:05:12 -0500

    modelo subscribers columna nombre --> name

--- api/src/models/Subscribers.js
+++ api/src/models/Subscribers.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
    sequelize.define('subscribers', {
-      nombre: {
+      name: {
          type: DataTypes.STRING,
          allowNull: false,
       },
