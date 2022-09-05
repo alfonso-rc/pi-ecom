d61bcb2fd93e6cc1c9510681b2986d9489975b06
Juani50 <juanibiondi50@gmail.com>
2022-09-05 17:10:28 -0300

    estilos en mis pedidos

--- client/src/componentes/MyShopping.jsx
+++ client/src/componentes/MyShopping.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import CardShopping from "./CardShopping";
 import { getShopping } from "../store/actions";
+import NavBarDetail from "./NavBarDetail";
+import Footer from "./Footer";
 
 export default function MisCompras() {
   const articulosShopping = useSelector((state) => state.shoppings);
@@ -16,9 +18,17 @@ export default function MisCompras() {
 
   return (
     <div>
+      <div>
+        <NavBarDetail/>
+      </div>
+      <div className="flex flex-row flex-wrap justify-evenly gap-y-11 gap-x-6 px-2 mx-auto sm:mx-56">
       {articulosShopping.map((art) => {
         return <CardShopping key={art.id} title={art.title} image={art.image} />;
       })}
+      </div>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
