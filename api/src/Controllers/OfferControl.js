d0d63d337e27e0dd7513500f5c76fe9f1d43f4fb
MarMerc <56653754+MarMerc@users.noreply.github.com>
2022-09-05 21:47:11 -0300

    Modelo Offer

--- api/src/Controllers/OfferControl.js
+++ api/src/Controllers/OfferControl.js
@@ -19,8 +19,11 @@ const testFunction = (req, res, next) => {
 const getAllOffers = async (req, res, next) => {
   try {
     let getAllOffers = await Offer.findAll({
-      attributes: ["id", "porcent", "validity","articleId"],
-      through: { attributes: [] }
+      include: {
+        model: Article,
+        attributes: ["title"],
+        through: { attributes: [] }
+      }
     });
     res.status(200).send(getAllComment);
   } catch (error) {
@@ -30,50 +33,11 @@ const getAllOffers = async (req, res, next) => {
 
 
 // CREATE ARTICLE USER RATING
-const createComment = async (req, res, next) => {
+const createOffer = async (req, res, next) => {
   const { idUser, articleId, texto } = req.body;
   console.log(req.body)
   try {
-    // Verificar si el idUser existe
-    const userComment = await User.findByPk(idUser)
-    if (!userComment) {
-      console.log("El usuario no existe en la base de datos")
-      res.status(404).send("El usuario no existe en la base de datos"); return
-    }
 
-    // Verificar si el artículo existe
-    const articleFound = await Article.findByPk(articleId)
-    if (!articleFound) {
-      console.log("El artículo no existe en la base de datos")
-      res.status(404).send("El artículo no existe en la base de datos"); return
-    }
-
-    // Verificar que el usuario no haya hecho ya un comentario a ese producto
-    const commentFound = await Comment.findOne({
-      where: {
-        userId: idUser,
-        articleId: articleId,
-      }
-    })
-    if (commentFound) {
-      console.log("El usuario ya ha hecho un comentario a este artículo")
-      res.status(400).send("El usuario ya ha hecho un comentario a este artículo"); return
-    }
-
-    // Verificar que el score se encuentre en el rango 1-5 y creamos el rating
-    if (texto) {
-      // Creamos el rating
-      const createdComment = await Comment.create({
-        texto: texto,
-        userId: idUser,
-        articleId: articleId
-      })
-
-      { res.status(200).send(createdComment.dataValues); return }
-    } else {
-      console.log("No hay texto para comentar")
-      res.status(400).send("No hay texto para comentar")
-    }
 
   } catch (error) {
     next(error);
--- client/src/componentes/Admin/Pages/ArticleList.jsx
+++ client/src/componentes/Admin/Pages/ArticleList.jsx
@@ -1,252 +1,196 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 //import React from "react";
-import {useSelector, useDispatch} from "react-redux";
-import {Link, history,useHistory} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, history, useHistory } from "react-router-dom";
 import s from "../Pages/articleList.module.css";
 import {
-	getAllArticles,
-	deleteArticle,
-	deleteArticleLogic,
-	editArticle,
+  getAllArticles,
+  deleteArticle,
+  deleteArticleLogic,
+  editArticle,
 } from "../../../../src/store/actions/index.js";
 
 export default function ArticleList() {
-	const [articulo, setArticulo] = useState([]);
-	const [tablaArticulo, setTablaArticulo] = useState([]);
-	const [busqueda, setBusqueda] = useState("");
-	const refreshPage = ()=>{
-		window.location.reload();
-	 }
-	let dispatch = useDispatch(); //
-	let allArticle = useSelector((state) => state.articles);
-	let user = sessionStorage;
-	const peticionGet = async () => {
-		await axios
-			.get("http://localhost:3001/article")
-			.then((response) => {
-				setArticulo(response.data);
-				setTablaArticulo(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+  const [articulo, setArticulo] = useState([]);
+  const [tablaArticulo, setTablaArticulo] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
 
-	const handleChange = (e) => {
-		setBusqueda(e.target.value);
-		filtrar(e.target.value);
-	};
+  const refreshPage = () => {
+    window.location.reload();
+  };
+  let dispatch = useDispatch(); //
+  let allArticle = useSelector((state) => state.articles);
+  let user = sessionStorage;
+  const peticionGet = async () => {
+    await axios
+      .get("http://localhost:3001/article")
+      .then((response) => {
+        setArticulo(response.data);
+        setTablaArticulo(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	const filtrar = (terminoBusqueda) => {
-		var resultadosBusqueda = tablaArticulo.filter((elemento) => {
-			if (
-				elemento.title
-					.toString()
-					.toLowerCase()
-					.includes(terminoBusqueda.toLowerCase()) ||
-				elemento.title
-					.toString()
-					.toLowerCase()
-					.includes(terminoBusqueda.toLowerCase())
-			) {
-				return elemento;
-			}
-		});
-		setArticulo(resultadosBusqueda);
-	};
+  const handleChange = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  };
 
-	useEffect(() => {
-		peticionGet();
-	}, []);
+  const filtrar = (terminoBusqueda) => {
+    var resultadosBusqueda = tablaArticulo.filter((elemento) => {
+      if (
+        elemento.title
+          .toString()
+          .toLowerCase()
+          .includes(terminoBusqueda.toLowerCase()) ||
+        elemento.title
+          .toString()
+          .toLowerCase()
+          .includes(terminoBusqueda.toLowerCase())
+      ) {
+        return elemento;
+      }
+    });
+    setArticulo(resultadosBusqueda);
+  };
 
-	function handleClickDelete(id) {
-		try {
-			dispatch(deleteArticle(id));
-			allArticle = allArticle.filter((a) => a.id !== id);
-			console.log(id);
-			alert(`El Articulo con id: ${id} fue Eliminado!`);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  useEffect(() => {
+    peticionGet();
+  }, []);
 
-	function handleClickInhab(id) {
-		try {
-			dispatch(deleteArticleLogic(id));
-			alert('Hecho!');
-			// history.push('/admin/articulos/');
-			refreshPage();
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  function handleClickDelete(id) {
+    try {
+      dispatch(deleteArticle(id));
+      allArticle = allArticle.filter((a) => a.id !== id);
+      console.log(id);
+      alert(`El Articulo con id: ${id} fue Eliminado!`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	function handleClickEdit(id) {
-		try {
-			dispatch(editArticle(id));
-			alert(`El Articulo con id: ${id} ha sido modificado!`);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  function handleClickInhab(id) {
+    try {
+      dispatch(deleteArticleLogic(id));
+      alert("Hecho!");
+      // history.push('/admin/articulos/');
+      refreshPage();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	return (
-		<div>
-			<h2 className="text-black text-2xl">
-				Bienvenido{" "}
-				{`${user.name[0].toUpperCase() + user.name.substring(1)}`} al
-				administrador del sitio
-			</h2>
-			<br />
-			<div>
-				<input
-					value={busqueda}
-					className="input input-bordered  text-white h-9 w-28 sm:w-full sm:h-12"
-					placeholder="Búsqueda por nombre"
-					onChange={handleChange}
-				/>
-			</div>
-			<div>
-				<div className={s.table}>
-					<br />
-					<table>
-						<thead>
-							<tr>
-								<th>Name</th>
-								<th>id</th>
-								<th>Marca</th>
-								<th>Modelo</th>
-								<th>Habilitado</th>
-								<th>Stock</th>
-								<th>Price</th>
-								<th>Action</th>
-								<th>Action</th>
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							{articulo &&
-								articulo.map((art) => {
-									return (
-										<tr>
-											<td>{art.title}</td>
-											<td>{art.id}</td>
-											<td>{art.detail.marca}</td>
-											<td>{art.detail.modelo}</td>
-											<td>{art.disable}</td>
-											<td>{art.stock}</td>
-											<td>{art.price}</td>
-											<td>
-												{art.disable ? (
-													<div>
-														<label className="swap">
-															<input type="checkbox" />
-															<div
-																className="swap-on btn btn-success btn-xs"
-																onClick={() =>
-																	handleClickInhab(
-																		art.id
-																	)
-																}
-															>
-																Habilitar
-															</div>
-															<div
-																className="swap-off btn btn-error btn-xs"
-																onClick={() =>
-																	handleClickInhab(
-																		art.id
-																	)
-																}
-															>
-																Deshabilitar
-															</div>
-														</label>
-													</div>
-												) : (
-													<div>
-														<label className="swap">
-															<input type="checkbox" />
-															<div
-																className="swap-off btn btn-error btn-xs"
-																onClick={() =>
-																	handleClickInhab(
-																		art.id
-																	)
-																}
-															>
-																Deshabilitar
-															</div>
-															<div
-																className="swap-on btn btn-success btn-xs"
-																onClick={() =>
-																	handleClickInhab(
-																		art.id
-																	)
-																}
-															>
-																Habilitar
-															</div>
-														</label>
-													</div>
-												)}
-											</td>
-											<td>
-												<Link
-													to={`/admin/articulos/edit/${art.id}`}
-												>
-													<button className="btn btn-info btn-xs">
-														Edit
-													</button>
-												</Link>
-											</td>
-											<td>
-												<a
-													href="#my-modal-2"
-													class="btn btn-error btn-xs"
-												>
-													Delete
-												</a>
-												<div
-													class="modal"
-													id="my-modal-2"
-												>
-													<div class="modal-box">
-														<h3 class="font-bold">
-															El Articulo se
-															eliminara de manera
-															permanente!
-														</h3>
-														<div class="modal-action">
-															<button
-																className="btn btn-error btn-xs"
-																onClick={() =>
-																	handleClickDelete(
-																		art.id
-																	)
-																}
-															>
-																Continuar
-															</button>
-															<a
-																href="#"
-																className="btn btn-xs"
-															>
-																Cancelar
-															</a>
-														</div>
-													</div>
-												</div>
-											</td>
-										</tr>
-									);
-								})}
-						</tbody>
-						<tfoot></tfoot>
-					</table>
-				</div>
-			</div>
-		</div>
-	);
+  function handleClickEdit(id) {
+    try {
+      dispatch(editArticle(id));
+      alert(`El Articulo con id: ${id} ha sido modificado!`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <h2 className="text-black text-2xl">Administrador de Articulos</h2>
+      <br />
+      <div>
+        <input
+          value={busqueda}
+          className="input input-bordered  text-white h-9 w-28 sm:w-full sm:h-12"
+          placeholder="Búsqueda por nombre"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <div className={s.table}>
+          <br />
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>id</th>
+                <th>Marca</th>
+                <th>Modelo</th>
+                <th>Habilitado</th>
+                <th>Stock</th>
+                <th>Price</th>
+                <th>Action</th>
+                <th>Action</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {articulo &&
+                articulo.map((art) => {
+                  return (
+                    <tr>
+                      <td>{art.title}</td>
+                      <td>{art.id}</td>
+                      <td>{art.detail.marca}</td>
+                      <td>{art.detail.modelo}</td>
+                      <td>{art.disable}</td>
+                      <td>{art.stock}</td>
+                      <td>{art.price}</td>
+                      <td>
+                        {!art.disable ? (
+                          <div>
+                            <button
+                              className="btn btn-success btn-xs"
+                              onClick={() => handleClickInhab(art.id)}
+                            >
+                              Habilitado
+                            </button>
+                          </div>
+                        ) : (
+                          <div>
+                            <button
+                              className="btn btn-error btn-xs"
+                              onClick={() => handleClickInhab(art.id)}
+                            >
+                              Deshabilitado
+                            </button>
+                          </div>
+                        )}
+                      </td>
+                      <td>
+                        <Link to={`/admin/articulos/edit/${art.id}`}>
+                          <button className="btn btn-info btn-xs">Edit</button>
+                        </Link>
+                      </td>
+                      <td>
+                        <a href="#my-modal-2" class="btn btn-error btn-xs">
+                          Delete
+                        </a>
+                        <div class="modal" id="my-modal-2">
+                          <div class="modal-box">
+                            <h3 class="font-bold">
+                              El Articulo se eliminara de manera permanente!
+                            </h3>
+                            <div class="modal-action">
+                              <button
+                                className="btn btn-error btn-xs"
+                                onClick={() => handleClickDelete(art.id)}
+                              >
+                                Continuar
+                              </button>
+                              <a href="#" className="btn btn-xs">
+                                Cancelar
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+            <tfoot></tfoot>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
