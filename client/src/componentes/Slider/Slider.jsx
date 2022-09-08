1438db624e72544e945ff1da64667bcaf33ec4fb
Alejandro Silva <alejoyodax@gmail.com>
2022-09-08 08:38:24 -0500

    fix bug rating detail

--- client/src/componentes/Slider/Slider.jsx
+++ client/src/componentes/Slider/Slider.jsx
@@ -2,11 +2,19 @@ import s from "./Slider.module.css"
 import { mostFavoriteArticles } from "./mostFavoriteArticles"  // LLegan por props
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios";
+
+const URL_GET_ARTICLES_FAVORITES =
+   process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_API_URL + "/article"
+      : "http://localhost:3001/article";
+
 
 export default function Slider() {
    const [actualSlide, setActualSlide] = useState(0)
    const itemsPerPage = window.innerWidth < 600 ? 2 : 4
    const timeInterval = 4300 //milisegundos
+   const [articles, setArticles] = useState([])
 
    // Para que se
    useEffect(() => {
@@ -16,6 +24,12 @@ export default function Slider() {
       return () => clearInterval(interval)   // Eliminar el intervalo cuando se desmonte
    }, [actualSlide] //
    )
+
+   useEffect(() => {
+      axios.get(URL_GET_ARTICLES_FAVORITES)
+         .then((res => setArticles(res.data.slice(0, 10))))
+         .catch(err => console.log("ERROR AL TRAER ARTÍCULOS EN SLIDER"))
+   }, [])
 
    function irAdelante() {
       console.log("ir adelante")
@@ -42,7 +56,7 @@ export default function Slider() {
          <div className={ s.containerSlider }>
             <div style={ { width: width, marginLeft: marginLeft } } id="slider" className={ s.slider }>
                {
-                  mostFavoriteArticles.map((item) => <SliderSection item={ item } />)
+                  articles.map((item) => <SliderSection item={ item } />)
                }
                <div onClick={ irAdelante } className={ s.slider_btn_right }>{ ">" }</div>
                <div onClick={ irAtras } className={ s.slider_btn_left }>{ "<" }</div>
