1b0f5bde065736b2ac068a189290105c10691626
MarMerc <56653754+MarMerc@users.noreply.github.com>
2022-09-08 01:55:51 -0300

    cosas

--- client/src/componentes/Admin/SideBarAdmin.jsx
+++ client/src/componentes/Admin/SideBarAdmin.jsx
@@ -7,7 +7,7 @@ const SideBarAdmin = () =>{
                 <Link to="/admin/articulos"><button className="border-b-2 border-violet-600 m-4 flex justify-start transition delay-100 hover:scale-110">Articulos</button></Link>
                 <Link to="/admin/usuarios"><button className="border-b-2 border-violet-600 m-4 flex justify-start transition delay-100 hover:scale-110">Usuarios</button></Link>
                 <Link to="/admin/ofertas"><button className="border-b-2 border-violet-600 m-4 flex justify-start transition delay-100 hover:scale-110">Ofertas</button></Link>
-                <Link to="/admin/St"><button className="border-b-2 border-violet-600 m-4 flex justify-start transition delay-100 hover:scale-110">Statistics</button></Link>
+                <Link to="/admin/St"><button className="border-b-2 border-violet-600 m-4 flex justify-start transition delay-100 hover:scale-110">Estadisticas</button></Link>
         </div>
     )
 }
