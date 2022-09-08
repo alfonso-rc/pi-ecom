57ff7ef4ec08f7c1ea5893e3fbd8ae63669fa553
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-09-07 22:00:34 -0500

    about

--- client/src/componentes/CardUs.jsx
+++ client/src/componentes/CardUs.jsx
@@ -67,7 +67,7 @@ export default function CardUs({}) {
 							target="_blank"
 							rel="noreferrer"
 							className="btn btn-primary"
-							href="#"
+							href="http://linkedin.com/in/nicol%C3%A1s-lautaro-zacar%C3%ADas-438a98216"
 						>
 							<BsLinkedin />
 						</a>
